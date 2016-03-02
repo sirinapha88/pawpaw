328b8280c844efaa4f5db7476142342f6a859258
sirinapha88 <sirinapha.dube@gmail.com>
2016-03-02 13:32:05 -0800

    add oauth.js

--- server/server.js
+++ server/server.js
@@ -7,3 +7,44 @@ var express 		= require("express"),
 	knex 			= require('../db/knex'),
 	passport 		= require('passport'),
 	FacebookStrategy	= require('passport-facebook').Strategy,
+	config = require('./oauth.js');
+	require('dotenv').load();
+
+var Users = function () {
+  return knex('users');
+};
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// config
+passport.use(new FacebookStrategy({
+  clientID: config.facebook.clientID,
+  clientSecret: config.facebook.clientSecret,
+  callbackURL: config.facebook.callbackURL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log(profile);
+    process.nextTick(function () {
+      Users().where({facebook_id: profile.id}).then(function(user, err) {
+        if(err)
+          done(err);
+        if(user[0]) {
+          return done(null, user[0]);
+        } else {
+          Users().insert({facebook_id: profile.id, name: profile.displayName, email: profile.emails}).then(function() {
+            Users().where({facebook_id: profile.id}).then(function(data) {
+              return done(null, data[0]);
+            });
+          });
+        }
+      });
+    });
+  }
+ ));
+ 
