734455090bb74b6568d73321894d056b50493898
sirinapha88 <sirinapha.dube@gmail.com>
2016-03-07 17:34:08 -0800

    made changed in app.js

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,16 @@
 var app = angular.module("pawpawHotel", ["ngRoute", "ngAnimate", "ngResource"]);
+
+app.config(function($routeProvider, $locationProvider, $httpProvider){
+	$routeProvider
+	.when("/", {
+		templateUrl: "client/views/templates/landing.html",
+		controller: "Landing",
+	})
+	.when("/login",{
+		templateUrl: "client/views/templates/login.html",
+		controller: "Login",
+	}).when("/dashboard", {
+		templateUrl: "client/views/templates/landing.html",
+		controller: "Dashboard",
+	})
+});
