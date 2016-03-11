a25b9095b0ec043f39bc32a1368744c25c97c9a1
sirinapha88 <sirinapha.dube@gmail.com>
2016-03-11 14:11:34 -0800

    modified users.js in server side

---
+++ server/routes/users.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var knex = require('../db/knex');
+var bcrypt = require('bcrypt');
+
+var Users = function () {
+  return knex('users');
+};
+
+router.get('/signup', function(req,res){
+
+});
+
+router.post('/signup', function(req,res){
+
+});
+
+router.get('/login', function(req,res){
+
+});
+
+router.post('/login', function(req,res){
+	
+});
+
+router.get('/profile', function(req,res){
+
+});
