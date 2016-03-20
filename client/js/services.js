1f331ec26e00040bca8349d62039c170a83cae9e
sirinapha88 <sirinapha.dube@gmail.com>
2016-03-19 19:19:58 -0700

    added loinService

--- client/js/services.js
+++ client/js/services.js
@@ -3,3 +3,14 @@ app.service('cartService', function(){
     	cart: []
   	};
 });
+
+app.service('loginService', function ($location,$http) {
+        
+        if (null != username && 0 != username.length && 
+                     null != password &&  0 != password.length) {
+            $scope.signedin = true;
+        } else {
+            $scope.signedin = false;
+        }
+    };
+});
