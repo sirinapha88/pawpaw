17728468f8f95fea514105952568d42c5bf714d1
sirinapha88 <sirinapha.dube@gmail.com>
2016-04-15 13:29:38 -0700

    modified google map

--- client/js/app.js
+++ client/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("pawpawHotel", ["ngRoute", "ngResource","ngAutocomplete","ngAnimate","uiGmapgoogle-maps", "angularMoment"]);
+var app = angular.module("pawpawHotel", ["ngRoute", "ngResource","ngAutocomplete","ngAnimate","uiGmapgoogle-maps"]);
 
 app.config(function($routeProvider, $locationProvider, $httpProvider){
 	$routeProvider
